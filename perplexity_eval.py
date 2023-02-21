ec39190953b24f6742ff2eba2e20b7acd21e395d
Faiail <raffaele.scaringi20@gmail.com>
2023-02-21 14:58:08 +0100

    performed perplexity

--- perplexity_eval.py
+++ perplexity_eval.py
@@ -7,11 +7,12 @@ import json
 
 
 class FastDataset(Dataset):
-    def __init__(self, data):
+    def __init__(self, data: pd.DataFrame, column: str = 'caption'):
         self.data = data
+        self.column = self.data.columns.tolist().index(column)
 
     def __getitem__(self, item):
-        return self.data.iloc[item, -1]
+        return [self.data.iloc[item, self.column]]
 
     def __len__(self):
         return len(self.data)
@@ -20,18 +21,19 @@ class FastDataset(Dataset):
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     data = pd.read_csv('artgraph/data/data_captions.csv', index_col=0)
-    dataset = FastDataset(data)
-    loader = DataLoader(dataset=dataset, batch_size=1, shuffle=False)
+    dataset = FastDataset(data, column='prompt')
+    loader = DataLoader(dataset=dataset, batch_size=8, shuffle=False)
 
-    model = AutoModelForCausalLM.from_pretrained('gpt2').to(device, non_blocking=True)
+    model = AutoModelForCausalLM.from_pretrained('gpt2').to(device)
     tokenizer = AutoTokenizer.from_pretrained('gpt2')
     tokenizer.pad_token = tokenizer.eos_token
 
     tot_loss = 0.0
     for captions in tqdm(loader):
-        captions = tokenizer(captions, return_tensors='pt', padding=True)['input_ids'].to(device, non_blocking=True)
+        captions = tokenizer(list(captions[0]), return_tensors='pt', padding=True)['input_ids'].to(device)
         loss = model(input_ids=captions, labels=captions).loss
-        tot_loss += loss
+        tot_loss += torch.exp(loss).item()
 
-    ppt = {'ppt': torch.exp(tot_loss/len(dataset))}
-    json.dump(ppt, open('ppt.json', 'w+'))
+    ppt = {'ppt': tot_loss/len(dataset)}
+    print(ppt)
+    json.dump(ppt, open('ppt_prompt.json', 'w+'))
